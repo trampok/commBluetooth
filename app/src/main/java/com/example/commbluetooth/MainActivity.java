71c7b1f643cb9998fc84805d81aa3489f3162593
trampok <thibault.fedhaoui@gmail.com>
2019-06-05 18:40:26 +0200

    Added permissions to the app. Added a button to the menu.

---
+++ app/src/main/java/com/example/commbluetooth/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.commbluetooth;
+
+import android.Manifest;
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.security.KeyStore;
+import java.security.Permission;
+
+import static android.os.Build.VERSION.SDK_INT;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    final static int NO_ADAPTER = 0;
+    final static int PERMISSIONS_GRANTED = 1;
+    final static int USER_REQUEST = 2;
+
+    final static int BT_ACTIVATION_REQUEST_CODE = 0;
+    final static int BT_ACTIVATION = 0;
+
+    public BluetoothAdapter mBluetoothAdapter;
+
+    final static String[] PERMISSIONS = new String[]{Manifest.permission.BLUETOOTH,Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int PERMISSIONS_REQUEST_CODE = 12;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItem = item.getItemId();
+        switch (menuItem){
+            case R.id.bluetooth_id:
+                switch ((BluetoothRight())){
+                    case NO_ADAPTER:
+                        Toast.makeText(this, "No Bluetooth adapter", Toast.LENGTH_LONG).show();
+                        break;
+                    case PERMISSIONS_GRANTED:
+                        //onBluetoothConfigRequest(false);
+                        break;
+                    case USER_REQUEST:
+                }
+        }
+        return true;
+    }
+
+   /* private void onBluetoothConfigRequest(boolean postActivationCall) {
+        Intent BTactivation,BTConnect;
+
+        if(!mBluetoothAdapter.isEnabled()){
+            if(!postActivationCall)
+        }
+
+    }*/
+
+    private int BluetoothRight() {
+        if (BluetoothAdapter.getDefaultAdapter() == null) {
+            return NO_ADAPTER;
+        }
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (SDK_INT >= Build.VERSION_CODES.N){
+            if (!checkMultiplePermissions(PERMISSIONS)) {
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+                return USER_REQUEST;
+            }
+        }
+        return PERMISSIONS_GRANTED;
+    }
+
+    private boolean checkMultiplePermissions(String[] permissions) {
+        if (permissions != null && permissions.length != 0){
+            for (String permission : permissions){
+                if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE){
+            for (int result : grantResults){
+                if (result == PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this, "Autorisation necessary", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+            //onBluetoothConfigRequest(false);
+        }
+    }
+}
+
+
+
